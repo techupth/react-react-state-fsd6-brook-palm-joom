58f84cbc5bbcc27bc9c5d7a1bc9a1286e7456c58
Joom <jnwongsa@gmail.com>
2024-06-05 13:47:26 +0700

    update:ex state/Joom

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [greeting, setGreeting] = useState("Greeting Message");
+  const handleClick = (message) => {
+    setGreeting(message);
+  };
+
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{greeting}</div>
       <div className="buttons">
-        <button>สวัสดี!</button>
-        <button>Hi!</button>
-        <button>你好!</button>
+        <button onClick={() => handleClick("สวัสดี!")}>สวัสดี!</button>
+        <button onClick={() => handleClick("Hi!")}>Hi!</button>
+        <button onClick={() => handleClick("你好!")}>你好!</button>
       </div>
     </div>
   );
